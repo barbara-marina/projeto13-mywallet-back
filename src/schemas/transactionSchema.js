c27769bde45a8c766ecb7b996303b37996b2b4e3
Vinicius <vinigugelmin@hotmail.com>
2022-05-17 12:45:26 -0300

    Preparando revisão de código

--- src/schemas/transactionSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const transactionSchema = joi.object({
-    amount: joi.number().required(),
-    description: joi.string().required(),
-    type: joi.string().valid("in", "out").required()
-});
-
-export default transactionSchema;
